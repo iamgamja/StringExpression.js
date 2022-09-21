d3824a264ab0d52225e0da25eca286ed2dbd29aa
Spotky1004 <spotky1004@gmail.com>
2022-09-21 22:07:02 +0900

    Clean /dist

--- dist/StringCode.js
+++
@@ -1,7 +0,0 @@
-import StringExpression from "./StringExpression.js";
-export default class StringCode extends StringExpression {
-    constructor(expressionCode) {
-        super();
-        const [variableNames] = expressionCode;
-    }
-}
